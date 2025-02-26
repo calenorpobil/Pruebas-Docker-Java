cba21f835ad22dd26d890f10a579a4eb3b629154
Marco Postman <89901767+calenorpobil@users.noreply.github.com>
2025-02-26 20:16:37 +0100

    Servidor funcional enviando y recibiendo archivos

--- ServidorSocket.java
+++ ServidorSocket.java
@@ -13,33 +13,18 @@ public class ServidorSocket {
 
             while (true) {
                 try (Socket socket = serverSocket.accept();
-                     DataInputStream inStream = new DataInputStream(socket.getInputStream())) {
+                     DataInputStream inStream = new DataInputStream(socket.getInputStream()))       
+                {
                     System.out.println("Cliente conectado: " + serverSocket.getInetAddress());
 
-
-                    // Recibir metadatos
-                    String nombreArchivo = inStream.readUTF();
-                    System.out.println("Nombre: " + nombreArchivo);
-                    long tamanyoArchivo = inStream.readLong();
-                    System.out.println("Tamanyo: " + tamanyoArchivo);
-
-                    // Preparar para guardar
-                    File archivoDestino = new File(DIRECTORIO_DESTINO + nombreArchivo);
-                    try (FileOutputStream fos = new FileOutputStream(archivoDestino);
-                         BufferedOutputStream bos = new BufferedOutputStream(fos)) {
-
-                        // Recibir y escribir bytes
-                        byte[] buffer = new byte[4096];
-                        int count;
-                        long totalRecibido = 0;
-                        while (totalRecibido < tamanyoArchivo && 
-                              (count = inStream.read(buffer)) != -1) {
-                            bos.write(buffer, 0, count);
-                            totalRecibido += count;
-                        }
-
-                        System.out.println("Archivo recibido: " + nombreArchivo);
+                    String opcion = inStream.readUTF();
+                    if(opcion.equals("ENVIAR")){
+                        recibirArchivoDeApp(serverSocket, inStream);
+                    }else if (opcion.equals("RECIBIR")){
+                        enviarArchivoAApp(serverSocket, socket, inStream);
                     }
+
+                    
 
                 } catch (IOException e) {
                     System.err.println("Error en conexión: " + e.getMessage());
@@ -47,6 +32,59 @@ public class ServidorSocket {
             }
         } catch (IOException e) {
             System.err.println("Error al iniciar servidor: " + e.getMessage());
+        }
+    }
+
+    private static void recibirArchivoDeApp(ServerSocket  socket, DataInputStream inStream) throws IOException{
+        // Recibir metadatos        
+        String nombreArchivo = inStream.readUTF();
+        System.out.println("Nombre: " + nombreArchivo);
+        long tamanyoArchivo = inStream.readLong();
+        System.out.println("Tamanyo: " + tamanyoArchivo);
+
+        // Preparar para guardar        
+        File archivoDestino = new File(DIRECTORIO_DESTINO + nombreArchivo);
+        try (FileOutputStream fos = new FileOutputStream(archivoDestino);
+        BufferedOutputStream bos = new BufferedOutputStream(fos)) {
+
+            // Recibir y escribir bytes
+
+            byte[] buffer = new byte[4096];
+            int count;
+            long totalRecibido = 0;
+            while (totalRecibido < tamanyoArchivo && (count = inStream.read(buffer)) != -1) {
+                bos.write(buffer, 0, count);
+                totalRecibido += count;
+            }
+
+            System.out.println("Archivo recibido: " + nombreArchivo);
+        }
+    }
+
+
+    public static void enviarArchivoAApp(ServerSocket  socket, Socket clientSocket, DataInputStream inStream) throws IOException {
+        String rutaArchivo = inStream.readUTF();
+        File archivo = new File(DIRECTORIO_DESTINO + File.separator +  rutaArchivo);
+        if (!archivo.exists()) {
+            throw new FileNotFoundException("Archivo no encontrado: " + rutaArchivo);
+        }
+
+        try (DataOutputStream outStream = new DataOutputStream(clientSocket.getOutputStream());
+             FileInputStream fis = new FileInputStream(archivo);
+             BufferedInputStream bis = new BufferedInputStream(fis)) {
+
+            // Enviar metadatos: nombre y tamaño
+            outStream.writeUTF(archivo.getName());
+            outStream.writeLong(archivo.length());
+
+            // Enviar archivo en bloques de 4 KB
+            byte[] buffer = new byte[4096];
+            int count;
+            while ((count = bis.read(buffer)) != -1) {
+                outStream.write(buffer, 0, count);
+            }
+
+            System.out.println("Archivo enviado: " + archivo.getName());
         }
     }
 }
